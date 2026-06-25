8ba4fb2e8d3dfd6289d60a987fea125297cb42e6
Kirill Astashov <kirill894343@gmail.com>
2026-06-25 12:51:50 +0300

    коммит

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-import http from "http";
+import express from "express";
+import cors from "cors";
+import fs from "fs";
 
-const server = http.createServer(function(request, response){
-    response.write("Text 1\n");
-    response.write("Text 2\n");
-    response.write("Hello Code AK\n");
-    response.end();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Главная страница
+app.get("/", (req, res) => {
+    res.json({ message: "Архитектурный блог API" });
 });
 
-server.listen(3000, "127.0.0.1", function() {
-    console.log("Сервер начал прослушивание запросов на порту 3000");
+// Получить все посты
+app.get("/posts", (req, res) => {
+    const posts = JSON.parse(fs.readFileSync("./data/posts.json", "utf-8"));
+    res.json(posts);
+});
+
+// Получить всех пользователей
+app.get("/users", (req, res) => {
+    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf-8"));
+    res.json(users);
+});
+
+app.listen(3000, () => {
+    console.log("Сервер архитектурного блога запущен на порту 3000");
 });
