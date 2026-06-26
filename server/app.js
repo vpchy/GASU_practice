db0c2b7df5693e5ddf478f97bc7e4365b3fda65f
Sonnik <sofiaart1373@gmail.com>
2026-06-26 12:33:27 +0300

    Сквош фронтенд-коммитов

---
+++ server/app.js
@@ -0,0 +1,13 @@
+import http from "http";
+
+const server = http.createServer(function(request, response){
+    response.write("Text 1\n");
+    response.write("Text 2\n");
+    response.write("Hello Code AK\n");
+    response.end();
+});
+
+server.listen(3000, "127.0.0.1", function() {
+    console.log("Сервер начал прослушивание запросов на порту 3000");
+});
+
