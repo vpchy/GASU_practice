89f45b51c238dc8fe6ff370c38a9e306529098cd
Kirill Astashov <kirill894343@gmail.com>
2026-06-26 20:49:40 +0300

    структура

---
+++ server/app.js
@@ -0,0 +1,295 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Архитектурный блог API" });
+});
+
+
+app.post("/login", (req, res) => {
+    const users = JSON.parse(
+        fs.readFileSync("./data/users.json", "utf-8")
+    );
+    let flag = false;
+
+    if (!req.body.login || !req.body.password) {
+        return res.json({
+            success: false,
+            message: "Введите логин и пароль"});
+        }
+    
+    for (let i = 0; i < users.length; i++) {
+        if ((users[i].login === req.body.login) && (users[i].password === req.body.password)) {
+            flag = true;
+            break;
+        }
+    }
+    if (flag) {
+        return res.json({
+            success: true,
+            message: "Вход успешен."   
+        });
+    } else {
+        return res.json({
+            success: false,
+            message: "Неправильный логин или пароль."
+        });
+    }
+});
+
+
+app.post("/register", (req, res) => {
+
+    const users = JSON.parse(
+        fs.readFileSync("./data/users.json", "utf-8")
+    );
+
+    if (!req.body.login || !req.body.password) {
+        return res.json({
+            success: false,
+            message: "Введите логин и пароль"
+        });
+    }
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].login === req.body.login) {
+            return res.json({
+                success: false,
+                message: "Такой пользователь уже существует"
+            });
+        }
+    }
+
+    const newUser = {
+        id: users.length + 1,
+        login: req.body.login,
+        password: req.body.password
+    };
+
+    users.push(newUser);
+
+    fs.writeFileSync(
+        "./data/users.json",
+        JSON.stringify(users, null, 4)
+    );
+
+    res.json({
+        success: true,
+        message: "Регистрация успешна"
+    });
+
+});
+
+
+app.get("/posts", (req, res) => {
+    const posts = JSON.parse(fs.readFileSync("./data/posts.json", "utf-8"));
+    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf-8"));
+
+    const result = posts.map(post => {
+        const author = users.find(u => u.id === post.authorId);
+
+        return {
+            id: post.id,
+            title: post.title,
+            text: post.text,
+            likes: post.likes,
+            author: author ? {
+                id: author.id,
+                login: author.login
+            } : null
+        };
+    });
+
+    return res.json(result);
+});
+
+app.post("/posts", (req, res) => {
+    const posts = JSON.parse(
+        fs.readFileSync("./data/posts.json", "utf-8")
+    );
+
+    if(!req.body.title || !req.body.text){
+        return res.json({
+            success: false,
+            message: "Введите заголовок и текст"
+        });
+    }
+    const newPost = {
+        id: posts.length + 1,
+        authorId: req.body.authorId,
+        title: req.body.title,
+        text: req.body.text,
+        likes: 0
+    };
+
+    posts.push(newPost);
+
+    fs.writeFileSync("./data/posts.json", JSON.stringify(posts, null, 4));
+
+    res.json({
+        
+        success: true,
+        message: "Пост отправлен",
+        
+    });
+
+})
+
+app.put("/posts/:id", (req, res) => {
+    const posts = JSON.parse(fs.readFileSync("./data/posts.json", "utf-8"));
+
+    if (!req.body.title || !req.body.text) {
+        return res.json({
+            success: false,
+            message: "Введите заголовок и текст"
+        });
+    }
+    
+    for(let i=0; i< posts.length; i++){
+        if (Number(req.params.id)=== posts[i].id){
+            posts[i].title = req.body.title;
+            posts[i].text = req.body.text;         
+
+            fs.writeFileSync("./data/posts.json", JSON.stringify(posts, null, 4));
+            return res.json({
+                success: true,
+                message: "Пост обновлен",
+            })
+        }
+    }
+    return res.json({
+        success: false,
+        message: "Пост не найден",
+    })
+
+})
+
+app.delete("/posts/:id", (req, res) =>{
+    const posts = JSON.parse(fs.readFileSync("./data/posts.json", "utf-8"));
+
+    for(let i=0; i< posts.length; i++){
+        if (Number(req.params.id)=== posts[i].id){
+            posts.splice(i,1);
+            fs.writeFileSync("./data/posts.json", JSON.stringify(posts, null, 4));
+            return res.json({
+                success: true,
+                message: "Пост удалён",
+            })
+
+        }
+    }
+    return res.json({
+        success: false,
+        message: "Пост не найден",
+    })
+})
+
+app.post("/posts/:id/like", (req, res) => {
+    const posts = JSON.parse(fs.readFileSync("./data/posts.json", "utf-8"));
+
+    for(let i=0; i< posts.length; i++){
+        if (Number(req.params.id)=== posts[i].id){
+            posts[i].likes++;
+            fs.writeFileSync("./data/posts.json", JSON.stringify(posts, null, 4));
+            return res.json({
+                success: true,
+                message: "Пост лайкнут",
+            })
+
+        }
+    }
+    return res.json({
+        success: false,
+        message: "Пост не найден",
+    })
+    
+})
+
+app.get("/posts/:id/comments", (req, res) => {
+    const comments = JSON.parse(fs.readFileSync("./data/comments.json", "utf-8"));
+    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf-8"));
+
+    const usersMap = new Map();
+
+    for (const user of users) {
+        usersMap.set(user.id, user);
+    }
+    const postId = Number(req.params.id);
+    const postComments = comments
+        .filter(c => c.postId === postId)
+        .map(comment => {
+            const user = usersMap.get(comment.authorId);
+
+            return {
+                id: comment.id,
+                text: comment.text,
+                author: user ? {
+                    id: user.id,
+                    login: user.login
+                } : null
+            };
+        });
+
+    return res.json(postComments);
+});
+
+app.post("/posts/:id/comments", (req, res) =>{
+    const posts = JSON.parse(fs.readFileSync("./data/posts.json", "utf-8"));
+    const comments = JSON.parse(fs.readFileSync("./data/comments.json", "utf-8"));
+    
+    for(let i=0; i< posts.length; i++){
+        if (Number(req.params.id)=== posts[i].id){
+            
+            const newComment = {
+                id: comments.length + 1,
+                postId: Number(req.params.id),
+                authorId: req.body.authorId,
+                text: req.body.text
+            };
+            comments.push(newComment);
+
+            fs.writeFileSync("./data/comments.json", JSON.stringify(comments, null, 4));
+            return res.json({
+                success: true,
+                message: "Комментарий отправлен.",
+            })
+
+        }
+    }
+    return res.json({
+        success: false,
+        message: "Пост не найден",
+    })
+})
+
+app.get("/users/:id", (req, res) => {
+    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf-8"));
+
+    const user = users.find(
+        u => u.id === Number(req.params.id)
+    );
+    if (!user) {
+        return res.json({
+            success: false,
+            message: "Пользователь не найден"
+        });
+    }
+
+    return res.json(user);
+})
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
